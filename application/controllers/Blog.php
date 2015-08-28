34894fbe846a52b978471a7306f8c45f331d5e41
Andrew Phommathep <andrew@starclubltd.com>
2015-08-27 17:42:43 -0700

    Adding Dynamic Data to the View

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -3,8 +3,11 @@ class Blog extends CI_Controller {
 
     public function index()
     {
+        $data['title'] = "My Real Title";
+        $data['heading'] = "My Real Heading";
+
 //        echo 'Hello World!';
-        $this->load->view('blogview');
+        $this->load->view('blogview', $data);
     }
 
     public function comments()
