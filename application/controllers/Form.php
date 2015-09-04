f53c57c06ec8b6b4ad99c8255390d7d97691567a
Andrew Phommathep <andrew@starclubltd.com>
2015-09-04 07:26:12 -0700

    Cascading Rules

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -8,12 +8,17 @@ class Form extends CI_Controller {
 
         $this->load->library('form_validation');
 
-        $this->form_validation->set_rules('username', 'Username', 'required');
-        $this->form_validation->set_rules('password', 'Password', 'required',
-            array('required' => 'You must provide a %s.')
+        $this->form_validation->set_rules(
+            'username', 'Username',
+            'required|min_length[5]|max_length[12]|is_unique[users.username]',
+            array(
+                'required'      => 'You have not provided %s.',
+                'is_unique'     => 'This %s already exists.'
+            )
         );
-        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
-        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
 
         if ($this->form_validation->run() == FALSE)
         {
