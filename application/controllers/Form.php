1a090a84bc8f94c45dba45493f3553949e543123
Andrew Phommathep <andrew@starclubltd.com>
2015-09-04 07:24:48 -0700

    Setting Validation Rules

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -8,6 +8,13 @@ class Form extends CI_Controller {
 
         $this->load->library('form_validation');
 
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required',
+            array('required' => 'You must provide a %s.')
+        );
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+
         if ($this->form_validation->run() == FALSE)
         {
             $this->load->view('myform');
