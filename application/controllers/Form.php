7b336e78c18d74f314a0327e0adfd7054288bfd6
Andrew Phommathep <andrew@starclubltd.com>
2015-09-04 07:27:01 -0700

    Prepping Data

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -8,17 +8,10 @@ class Form extends CI_Controller {
 
         $this->load->library('form_validation');
 
-        $this->form_validation->set_rules(
-            'username', 'Username',
-            'required|min_length[5]|max_length[12]|is_unique[users.username]',
-            array(
-                'required'      => 'You have not provided %s.',
-                'is_unique'     => 'This %s already exists.'
-            )
-        );
-        $this->form_validation->set_rules('password', 'Password', 'required');
-        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[12]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[8]');
+        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required|matches[password]');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
 
         if ($this->form_validation->run() == FALSE)
         {
